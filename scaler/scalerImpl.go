076ba1aa23e2920149c3db4506e25872b985b5a2
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-02-06 20:00:45 +0100

    .Updated extracted method

--- scaler/scalerImpl.go
+++ scaler/scalerImpl.go
@@ -11,14 +11,57 @@ type jobConfig struct {
 	maxCount uint
 }
 
-// ScaleBy Scales the target component by the given amount of instances
-func (s *Scaler) ScaleBy(amount int) error {
-	jobName := s.job.jobName
+type policyCheckResult struct {
+	validCount        uint
+	desiredCount      uint
+	minPolicyViolated bool
+	maxPolicyViolated bool
+}
 
+func amountToScaleType(amount int) string {
 	scaleTypeStr := "UP"
 	if amount < 0 {
 		scaleTypeStr = "DOWN"
 	}
+	return scaleTypeStr
+}
+
+func checkScalingPolicy(count uint, amount int, min uint, max uint) policyCheckResult {
+
+	result := policyCheckResult{minPolicyViolated: false, maxPolicyViolated: false}
+
+	newCountTmp := int(count) + amount
+	newCount := uint(newCountTmp)
+	if newCountTmp < 0 {
+		newCount = 0
+	}
+
+	result.desiredCount = newCount
+
+	// check if count exceeds minimum
+	if newCount < min {
+		newCount = min
+		result.minPolicyViolated = true
+	}
+
+	// check if count exceeds maximum
+	if newCount > max {
+		newCount = max
+		result.maxPolicyViolated = true
+	}
+
+	result.validCount = newCount
+
+	return result
+}
+
+// ScaleBy Scales the target component by the given amount of instances
+func (s *Scaler) ScaleBy(amount int) error {
+	jobName := s.job.jobName
+	min := s.job.minCount
+	max := s.job.maxCount
+	scaleTypeStr := amountToScaleType(amount)
+
 	s.logger.Info().Str("job", jobName).Msgf("Request to scale job %s by %d.", scaleTypeStr, amount)
 
 	dead, err := s.scalingTarget.IsJobDead(jobName)
@@ -35,23 +78,13 @@ func (s *Scaler) ScaleBy(amount int) error {
 		return fmt.Errorf("Error obtaining job count: %s.", err.Error())
 	}
 
-	newCountTmp := int(count) + amount
-	newCount := uint(newCountTmp)
-	if newCountTmp < 0 {
-		newCount = 0
+	chkResult := checkScalingPolicy(count, amount, min, max)
+	newCount := chkResult.validCount
+	if chkResult.minPolicyViolated {
+		s.logger.Info().Str("job", jobName).Msgf("Job.MinCount (%d) policy violated (wanted %d, have %d). Scale %s limited to %d.", min, chkResult.desiredCount, count, scaleTypeStr, newCount)
 	}
-	countWanted := newCount
-
-	// check if count exceeds minimum
-	if newCount < s.job.minCount {
-		newCount = s.job.minCount
-		s.logger.Info().Str("job", jobName).Msgf("Job.MinCount (%d) policy violated (wanted %d, have %d). Scale %s limited to %d.", s.job.minCount, countWanted, count, scaleTypeStr, newCount)
-	}
-
-	// check if count exceeds maximum
-	if newCount > s.job.maxCount {
-		newCount = s.job.maxCount
-		s.logger.Info().Str("job", jobName).Msgf("Job.MaxCount (%d) policy violated (wanted %d, have %d). Scale %s limited to %d.", s.job.maxCount, countWanted, count, scaleTypeStr, newCount)
+	if chkResult.maxPolicyViolated {
+		s.logger.Info().Str("job", jobName).Msgf("Job.MaxCount (%d) policy violated (wanted %d, have %d). Scale %s limited to %d.", max, chkResult.desiredCount, count, scaleTypeStr, newCount)
 	}
 
 	diff := int(math.Abs(float64(newCount) - float64(count)))
