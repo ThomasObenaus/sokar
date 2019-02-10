dde65e4554995194848d735e233caf9c42b2da30
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-02-10 13:27:49 +0100

    .Fix fixed failed unittest

--- scaler/scaler_test.go
+++ scaler/scaler_test.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/thomasobenaus/sokar/sokar"
 	"github.com/thomasobenaus/sokar/test/scaler"
 )
 
@@ -37,16 +39,17 @@ func TestScaleBy_JobDead(t *testing.T) {
 	jobname := "any"
 	cfg := Config{JobName: jobname}
 	scaler, err := cfg.New(scaTgt)
+	require.NoError(t, err)
 
 	// dead job - error
 	scaTgt.EXPECT().IsJobDead(jobname).Return(false, fmt.Errorf("internal error"))
-	err = scaler.ScaleBy(2)
-	assert.Error(t, err)
+	result := scaler.ScaleBy(2)
+	assert.Equal(t, sokar.ScaleFailed, result.State)
 
 	// dead job
 	scaTgt.EXPECT().IsJobDead(jobname).Return(true, nil)
-	err = scaler.ScaleBy(2)
-	assert.Error(t, err)
+	result = scaler.ScaleBy(2)
+	assert.Equal(t, sokar.ScaleIgnored, result.State)
 }
 
 func TestScaleBy_Up(t *testing.T) {
@@ -59,30 +62,31 @@ func TestScaleBy_Up(t *testing.T) {
 	jobname := "any"
 	cfg := Config{JobName: jobname, MinCount: 1, MaxCount: 5}
 	scaler, err := cfg.New(scaTgt)
+	require.NoError(t, err)
 
 	// scale up
 	currentJobCount := uint(0)
 	scaTgt.EXPECT().IsJobDead(jobname).Return(false, nil)
 	scaTgt.EXPECT().GetJobCount(jobname).Return(currentJobCount, nil)
 	scaTgt.EXPECT().SetJobCount(jobname, uint(2)).Return(nil)
-	err = scaler.ScaleBy(2)
-	assert.NoError(t, err)
+	result := scaler.ScaleBy(2)
+	assert.NotEqual(t, sokar.ScaleFailed, result.State)
 
 	// scale up - relative
 	currentJobCount = uint(1)
 	scaTgt.EXPECT().IsJobDead(jobname).Return(false, nil)
 	scaTgt.EXPECT().GetJobCount(jobname).Return(currentJobCount, nil)
 	scaTgt.EXPECT().SetJobCount(jobname, uint(3)).Return(nil)
-	err = scaler.ScaleBy(2)
-	assert.NoError(t, err)
+	result = scaler.ScaleBy(2)
+	assert.NotEqual(t, sokar.ScaleFailed, result.State)
 
 	// scale up - max hit
 	currentJobCount = uint(4)
 	scaTgt.EXPECT().IsJobDead(jobname).Return(false, nil)
 	scaTgt.EXPECT().GetJobCount(jobname).Return(currentJobCount, nil)
 	scaTgt.EXPECT().SetJobCount(jobname, uint(5)).Return(nil)
-	err = scaler.ScaleBy(2)
-	assert.NoError(t, err)
+	result = scaler.ScaleBy(2)
+	assert.NotEqual(t, sokar.ScaleFailed, result.State)
 }
 
 func TestScaleBy_Down(t *testing.T) {
@@ -95,22 +99,23 @@ func TestScaleBy_Down(t *testing.T) {
 	jobname := "any"
 	cfg := Config{JobName: jobname, MinCount: 1, MaxCount: 5}
 	scaler, err := cfg.New(scaTgt)
+	require.NoError(t, err)
 
 	// scale down
 	currentJobCount := uint(3)
 	scaTgt.EXPECT().IsJobDead(jobname).Return(false, nil)
 	scaTgt.EXPECT().GetJobCount(jobname).Return(currentJobCount, nil)
 	scaTgt.EXPECT().SetJobCount(jobname, uint(1)).Return(nil)
-	err = scaler.ScaleBy(-2)
-	assert.NoError(t, err)
+	result := scaler.ScaleBy(-2)
+	assert.NotEqual(t, sokar.ScaleFailed, result.State)
 
 	// scale up - min hit
 	currentJobCount = uint(2)
 	scaTgt.EXPECT().IsJobDead(jobname).Return(false, nil)
 	scaTgt.EXPECT().GetJobCount(jobname).Return(currentJobCount, nil)
 	scaTgt.EXPECT().SetJobCount(jobname, uint(1)).Return(nil)
-	err = scaler.ScaleBy(-5)
-	assert.NoError(t, err)
+	result = scaler.ScaleBy(-5)
+	assert.NotEqual(t, sokar.ScaleFailed, result.State)
 }
 
 func TestScaleBy_NoScale(t *testing.T) {
@@ -123,11 +128,12 @@ func TestScaleBy_NoScale(t *testing.T) {
 	jobname := "any"
 	cfg := Config{JobName: jobname, MinCount: 1, MaxCount: 5}
 	scaler, err := cfg.New(scaTgt)
+	require.NoError(t, err)
 
 	// scale down
 	currentJobCount := uint(5)
 	scaTgt.EXPECT().IsJobDead(jobname).Return(false, nil)
 	scaTgt.EXPECT().GetJobCount(jobname).Return(currentJobCount, nil)
-	err = scaler.ScaleBy(2)
-	assert.NoError(t, err)
+	result := scaler.ScaleBy(2)
+	assert.NotEqual(t, sokar.ScaleFailed, result.State)
 }
