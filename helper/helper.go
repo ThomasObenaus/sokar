58c09582197224e848875f3b5c08af6159f5adc3
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-02-11 21:12:25 +0100

    .Update added small helper for increment

---
+++ helper/helper.go
@@ -0,0 +1,34 @@
+package helper
+
+const (
+	maxUint = ^uint(0)
+	minUint = 0
+	maxInt  = int(maxUint >> 1)
+	minInt  = -maxInt - 1
+)
+
+// IncUint increments the given value (unsigned) by the given amount (signed)
+// and returns the result as unsigned int (safe from under-/overflow)
+func IncUint(value uint, by int) uint {
+
+	// upper thresh of uint reached
+	// avoid overflow
+	if value >= maxUint && by >= 0 {
+		return maxUint
+	}
+
+	// positive by --> just add
+	if by >= 0 {
+		return value + uint(by)
+	}
+
+	// negative by --> convert to uint and substract
+	byUint := uint(by * -1)
+
+	// avoid underflow
+	if byUint >= value {
+		return 0
+	}
+
+	return value - byUint
+}
