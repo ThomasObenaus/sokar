8ef2a6b6ba3c80931c901b95a57c1bd234fdfa1f
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-02-24 16:45:24 +0100

    .Update added scaling based on weight

--- scaleEventAggregator/aggregate.go
+++ scaleEventAggregator/aggregate.go
@@ -4,18 +4,19 @@ func (sc *ScaleEventAggregator) aggregate() {
 	sc.logger.Info().Msg("Aggregate")
 	sc.logPool()
 
-	scaleUp := 0
-	sc.scaleAlertPool.iterate(func(key uint32, entry ScaleAlertPoolEntry) {
-		scaleUp++
-	})
+	sc.scaleAlertPool.iterate(sc.updateScaleCounter)
 
 	// FIXME: This currently blocks until the deployment is done
-	if scaleUp > 0 {
-		sc.logger.Info().Msg("Scale UP by 1")
+	if sc.scaleCounter > sc.upScalingThreshold {
+		sc.logger.Info().Msgf("Scale UP by 1 because upscalingThreshold (%f) was violated. ScaleCounter is currently %f", sc.upScalingThreshold, sc.scaleCounter)
+		sc.scaleCounter = 0
 		sc.emitScaleEvent(1)
-	} else {
-		sc.logger.Info().Msg("Scale DOWN by 1")
+	} else if sc.scaleCounter < sc.downScalingThreshold {
+		sc.logger.Info().Msgf("Scale DOWN by 1 because downScalingThreshold (%f) was violated. ScaleCounter is currently %f", sc.downScalingThreshold, sc.scaleCounter)
+		sc.scaleCounter = 0
 		sc.emitScaleEvent(-1)
+	} else {
+		sc.logger.Info().Msgf("No scaling needed. ScaleCounter is currently %f [%f/%f].", sc.scaleCounter, sc.downScalingThreshold, sc.upScalingThreshold)
 	}
 }
 
@@ -25,4 +26,28 @@ func (sc *ScaleEventAggregator) logPool() {
 	sc.scaleAlertPool.iterate(func(key uint32, entry ScaleAlertPoolEntry) {
 		sc.logger.Debug().Str("name", entry.scaleAlert.Name).Str("receiver", entry.receiver).Msgf("[%d] fire=%t,start=%s,exp=%s", key, entry.scaleAlert.Firing, entry.scaleAlert.StartedAt.String(), entry.expiresAt.String())
 	})
+}
+
+// getWeight returns the scale weight for the given alert.
+// 0 is returned in case the weight for the given alert is not defined in the map
+func getWeight(alertName string, weightMap ScaleAlertWeightMap) float32 {
+	w, ok := weightMap[alertName]
+	if !ok {
+		return 0
+	}
+
+	return w
+}
+
+func (sc *ScaleEventAggregator) updateScaleCounter(key uint32, entry ScaleAlertPoolEntry) {
+	// ignore resolved alerts
+	if !entry.scaleAlert.Firing {
+		return
+	}
+
+	alertName := entry.scaleAlert.Name
+	weight := getWeight(alertName, sc.weightMap)
+	sc.scaleCounter += weight
+
+	sc.logger.Debug().Msgf("ScaleCounter updated by %f to %f because of scaling-alert %s", weight, sc.scaleCounter, alertName)
 }
