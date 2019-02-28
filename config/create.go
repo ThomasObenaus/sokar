5944d67ab64d96a05b76cbfaad83e4598e41ec29
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-02-28 17:19:12 +0100

    .Update added logging and job config

--- config/create.go
+++ config/create.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"io"
+	"time"
 
 	"gopkg.in/yaml.v2"
 )
@@ -12,4 +13,21 @@ func NewConfigFromYAML(reader io.Reader) (Config, error) {
 	cfg := Config{}
 	err := yaml.NewDecoder(reader).Decode(&cfg)
 	return cfg, err
+}
+
+// NewDefaultConfig returns a default configuration without any alerts (mappings)
+// or server configuration defined.
+func NewDefaultConfig() Config {
+	return Config{
+		Logging{Structured: false, UxTimestamp: false},
+		Job{},
+		ScaleAlertAggregator{
+			EvaluationCycle:        time.Second * 1,
+			EvaluationPeriodFactor: 10,
+			CleanupCycle:           time.Second * 60,
+			NoAlertScaleDamping:    1,
+			UpScaleThreshold:       10,
+			DownScaleThreshold:     -10,
+		},
+	}
 }
--- config/create_test.go
+++ config/create_test.go
@@ -3,36 +3,79 @@ package config
 import (
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/davecgh/go-spew/spew"
 )
 
-var data = `
-alerts:
-  - name: "AlertA"
-    weight: 1.5
-  - name: "AlertB"
-    weight: -1.5
-    description: "Down alert"
+var fullConfig = `
+job:
+  name: "fail-service"
+  min: 1
+  max: 10
+scale_alert_aggregator:
+  no_alert_damping: 1.0
+  up_thresh: 10.0
+  down_thresh: -10.0
+  eval_cycle: 1s
+  eval_period_factor: 10
+  cleanup_cycle: 60s
+  scale_alerts:
+    - name: "AlertA"
+      weight: 1.5
+    - name: "AlertB"
+      weight: -1.5
+      description: "Down alert"
+logging:
+  structured: false
+  unix_ts: false
 `
 
-func Test_configFromYAML(t *testing.T) {
+func Test_NewconfigFromYAML(t *testing.T) {
 
-	reader := strings.NewReader(data)
+	reader := strings.NewReader(fullConfig)
 
 	config, err := NewConfigFromYAML(reader)
 	assert.NoError(t, err)
-	assert.Len(t, config.Alerts, 2)
-	assert.Equal(t, "AlertA", config.Alerts[0].Name)
-	assert.Equal(t, float32(1.5), config.Alerts[0].Weight)
-	assert.Equal(t, "", config.Alerts[0].Description)
 
-	assert.Equal(t, "AlertB", config.Alerts[1].Name)
-	assert.Equal(t, float32(-1.5), config.Alerts[1].Weight)
-	assert.Equal(t, "Down alert", config.Alerts[1].Description)
+	// logging
+	assert.False(t, config.Logging.Structured)
+	assert.False(t, config.Logging.UxTimestamp)
+
+	// job
+	assert.Equal(t, "fail-service", config.Job.Name)
+	assert.Equal(t, uint(1), config.Job.MinCount)
+	assert.Equal(t, uint(10), config.Job.MaxCount)
+
+	// cfg
+	assert.Equal(t, float32(1), config.ScaleAlertAggregator.NoAlertScaleDamping)
+	assert.Equal(t, float32(10), config.ScaleAlertAggregator.UpScaleThreshold)
+	assert.Equal(t, float32(-10), config.ScaleAlertAggregator.DownScaleThreshold)
+	assert.Equal(t, time.Second*1, config.ScaleAlertAggregator.EvaluationCycle)
+	assert.Equal(t, uint(10), config.ScaleAlertAggregator.EvaluationPeriodFactor)
+	assert.Equal(t, time.Second*60, config.ScaleAlertAggregator.CleanupCycle)
+
+	// scale_alerts
+	assert.Len(t, config.ScaleAlertAggregator.ScaleAlerts, 2)
+	assert.Equal(t, "AlertA", config.ScaleAlertAggregator.ScaleAlerts[0].Name)
+	assert.Equal(t, float32(1.5), config.ScaleAlertAggregator.ScaleAlerts[0].Weight)
+	assert.Equal(t, "", config.ScaleAlertAggregator.ScaleAlerts[0].Description)
+	assert.Equal(t, "AlertB", config.ScaleAlertAggregator.ScaleAlerts[1].Name)
+	assert.Equal(t, float32(-1.5), config.ScaleAlertAggregator.ScaleAlerts[1].Weight)
+	assert.Equal(t, "Down alert", config.ScaleAlertAggregator.ScaleAlerts[1].Description)
 
 	spew.Dump(config)
 
+}
+
+func Test_NewDefaultConfig(t *testing.T) {
+	config := NewDefaultConfig()
+	assert.Equal(t, float32(1), config.ScaleAlertAggregator.NoAlertScaleDamping)
+	assert.Equal(t, float32(10), config.ScaleAlertAggregator.UpScaleThreshold)
+	assert.Equal(t, float32(-10), config.ScaleAlertAggregator.DownScaleThreshold)
+	assert.Equal(t, time.Second*1, config.ScaleAlertAggregator.EvaluationCycle)
+	assert.Equal(t, uint(10), config.ScaleAlertAggregator.EvaluationPeriodFactor)
+	assert.Equal(t, time.Second*60, config.ScaleAlertAggregator.CleanupCycle)
 }
