40270a719e9b8bdfaa1c19e8bbb147b3c33631a6
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-02-28 11:21:09 +0100

    .Update added getter for the gradient

--- helper/latestGradient_test.go
+++ helper/latestGradient_test.go
@@ -24,3 +24,32 @@ func Test_UpdateAndGet(t *testing.T) {
 	assert.Equal(t, float32(-1), lg.UpdateAndGet(-10, ago40Secs))
 	assert.Equal(t, float32(1), lg.UpdateAndGet(0, ago30Secs))
 }
+
+func Test_Update(t *testing.T) {
+
+	lg := LatestGradient{}
+	now := time.Now()
+	ago60Secs := now.Add(time.Second * -60)
+
+	lg.Update(0, ago60Secs)
+	assert.Equal(t, float32(0), lg.Value)
+	assert.Equal(t, ago60Secs, lg.Timestamp)
+
+	lg.Update(10, ago60Secs)
+	assert.Equal(t, float32(10), lg.Value)
+	assert.Equal(t, ago60Secs, lg.Timestamp)
+}
+
+func Test_Get(t *testing.T) {
+
+	lg := LatestGradient{}
+	now := time.Now()
+	ago60Secs := now.Add(time.Second * -60)
+	ago50Secs := now.Add(time.Second * -50)
+	ago40Secs := now.Add(time.Second * -40)
+
+	lg.Update(0, ago50Secs)
+	assert.Equal(t, float32(1), lg.Get(10, ago40Secs))
+	assert.Equal(t, float32(0), lg.Get(0, ago40Secs))
+	assert.Equal(t, float32(0), lg.Get(10, ago60Secs))
+}
