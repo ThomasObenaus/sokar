7ac257f2223744ed8e5bbc6e36c2a62da5865c6e
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-02-28 11:21:09 +0100

    .Update added getter for the gradient

--- helper/latestGradient.go
+++ helper/latestGradient.go
@@ -11,7 +11,7 @@ type LatestGradient struct {
 	Value     float32
 	Timestamp time.Time
 
-	lock sync.Mutex
+	lock sync.RWMutex
 }
 
 // UpdateAndGet is a thread safe method to calculate the gradient between
@@ -19,14 +19,32 @@ type LatestGradient struct {
 // With each call to UpdateAndGet the given value and timestamp are stored internally.
 // Furthermore the gradient is calculated by (currentValue-lastValue)/(currentTimeStamp-lastTimestamp).
 func (lg *LatestGradient) UpdateAndGet(value float32, timestamp time.Time) float32 {
+	result := lg.Get(value, timestamp)
+
+	lg.lock.Lock()
+	defer lg.lock.Unlock()
+	lg.Value = value
+	lg.Timestamp = timestamp
+
+	return result
+}
+
+// Update sets new values for the gradient.
+func (lg *LatestGradient) Update(value float32, timestamp time.Time) {
 	lg.lock.Lock()
 	defer lg.lock.Unlock()
 
-	increment := value - lg.Value
-	timeSpan := timestamp.Sub(lg.Timestamp)
-
 	lg.Value = value
 	lg.Timestamp = timestamp
+}
+
+// Get returns the gradient.
+func (lg *LatestGradient) Get(value float32, timestamp time.Time) float32 {
+	lg.lock.RLock()
+	defer lg.lock.RUnlock()
+
+	increment := value - lg.Value
+	timeSpan := timestamp.Sub(lg.Timestamp)
 
 	if increment == 0 {
 		return 0
@@ -37,13 +55,4 @@ func (lg *LatestGradient) UpdateAndGet(value float32, timestamp time.Time) float
 	}
 
 	return float32(float64(increment) / timeSpan.Seconds())
-}
-
-// Update sets new values for the gradient.
-func (lg *LatestGradient) Update(value float32, timestamp time.Time) {
-	lg.lock.Lock()
-	defer lg.lock.Unlock()
-
-	lg.Value = value
-	lg.Timestamp = timestamp
 }
