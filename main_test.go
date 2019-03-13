97a12edcf72df93a601d1a91681e64c9b707e678
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-03-13 20:50:57 +0100

    .Update setup logging

--- main_test.go
+++ main_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/thomasobenaus/sokar/config"
 )
 
 func Test_CliAndConfig(t *testing.T) {
@@ -16,17 +17,31 @@ func Test_CliAndConfig(t *testing.T) {
 	cfg, err := cliAndConfig(args)
 
 	require.NoError(t, err)
+	assert.NotNil(t, cfg)
 	assert.Equal(t, nomadSrvAddr, cfg.Nomad.ServerAddr)
 
 	args = []string{"./sokar-bin", "--co"}
 	cfg, err = cliAndConfig(args)
 	assert.Error(t, err)
+	assert.Nil(t, cfg)
 
 	args = []string{"./sokar-bin"}
 	cfg, err = cliAndConfig(args)
 	assert.Error(t, err)
+	assert.Nil(t, cfg)
 
 	args = []string{"./sokar-bin", "--config-file=" + cfgFile}
 	cfg, err = cliAndConfig(args)
 	assert.Error(t, err)
+	assert.Nil(t, cfg)
+}
+
+func Test_SetupLogging(t *testing.T) {
+	_, err := setupLogging(nil)
+	assert.Error(t, err)
+
+	cfg := config.Config{}
+	lf, err := setupLogging(&cfg)
+	assert.NoError(t, err)
+	assert.NotNil(t, lf)
 }
