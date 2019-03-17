4782a3d8b73dac14ed020daf04c111138408ebc3
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-03-17 12:18:05 +0100

    .Update setupScaler + test

--- main_test.go
+++ main_test.go
@@ -3,9 +3,11 @@ package main
 import (
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/thomasobenaus/sokar/config"
+	"github.com/thomasobenaus/sokar/test/logging"
 )
 
 func Test_CliAndConfig(t *testing.T) {
@@ -44,4 +46,26 @@ func Test_SetupLogging(t *testing.T) {
 	lf, err := setupLogging(&cfg)
 	assert.NoError(t, err)
 	assert.NotNil(t, lf)
+}
+
+func Test_SetupScaler(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	logF := mock_logging.NewMockLoggerFactory(mockCtrl)
+
+	// no logging factory
+	scaler, err := setupScaler("any", 0, 1, "nomad-addr", nil)
+	assert.Error(t, err)
+	assert.Nil(t, scaler)
+
+	logF.EXPECT().NewNamedLogger(gomock.Any()).Times(1)
+	scaler, err = setupScaler("any", 0, 1, "", logF)
+	assert.Error(t, err)
+	assert.Nil(t, scaler)
+
+	logF.EXPECT().NewNamedLogger(gomock.Any()).Times(2)
+	scaler, err = setupScaler("any", 0, 1, "https://nomad.com", logF)
+	assert.NoError(t, err)
+	assert.NotNil(t, scaler)
 }
