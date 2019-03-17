74afbbfeb567ee7e5c806f5255b7e7ceccfb83b0
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-03-18 00:27:32 +0100

    .Fix crev

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -1,19 +1,23 @@
 package metrics
 
+// Counter represents a counter metric
 type Counter interface {
 	Inc()
 	Add(float64)
 }
 
+// Gauge represents a gauge metric
 type Gauge interface {
 	Set(float64)
 	Add(float64)
 }
 
+// GaugeVec represents a vector of labelled gauges
 type GaugeVec interface {
 	WithLabelValues(lvs ...string) Gauge
 }
 
+// CounterVec represents a vector of labelled counters
 type CounterVec interface {
 	WithLabelValues(lvs ...string) Counter
 }
