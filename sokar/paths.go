d27c916d88a2b6da01cceda0c4a263e941813383
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-03-17 20:43:18 +0100

    .Update added unittest for sokar handle scale event

---
+++ sokar/paths.go
@@ -0,0 +1,12 @@
+package sokar
+
+const (
+	// PathHealth is the url path for health end-point
+	PathHealth = "/health"
+
+	// PathMetrics path for the metrics end-point
+	PathMetrics = "/metrics"
+
+	// PathAlertmanager path for receiving alerts from the alertmanager
+	PathAlertmanager = "/alerts"
+)
