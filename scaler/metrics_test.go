d7ac97debbc51081a41d82b5f79b06ec8f08e80e
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-03-24 19:21:44 +0100

    .Update added metric for scale result

--- scaler/metrics_test.go
+++ scaler/metrics_test.go
@@ -11,6 +11,7 @@ import (
 type MetricsMocks struct {
 	scalingPolicyViolated *mock_metrics.MockCounterVec
 	scalingTicketCount    *mock_metrics.MockCounterVec
+	scaleResultCounter    *mock_metrics.MockCounterVec
 }
 
 // NewMockedMetrics creates and returns mocked metrics that can be used
@@ -24,13 +25,16 @@ type MetricsMocks struct {
 func NewMockedMetrics(mockCtrl *gomock.Controller) (Metrics, MetricsMocks) {
 	mScalingPolicyViolated := mock_metrics.NewMockCounterVec(mockCtrl)
 	mScalingTicketCount := mock_metrics.NewMockCounterVec(mockCtrl)
+	mScaleResultCounter := mock_metrics.NewMockCounterVec(mockCtrl)
 	metrics := Metrics{
 		scalingPolicyViolated: mScalingPolicyViolated,
 		scalingTicketCount:    mScalingTicketCount,
+		scaleResultCounter:    mScaleResultCounter,
 	}
 	mocks := MetricsMocks{
 		scalingPolicyViolated: mScalingPolicyViolated,
 		scalingTicketCount:    mScalingTicketCount,
+		scaleResultCounter:    mScaleResultCounter,
 	}
 	return metrics, mocks
 }
@@ -39,4 +43,5 @@ func Test_NewMetrics(t *testing.T) {
 	metrics := NewMetrics()
 	assert.NotNil(t, metrics.scalingPolicyViolated)
 	assert.NotNil(t, metrics.scalingTicketCount)
+	assert.NotNil(t, metrics.scaleResultCounter)
 }
