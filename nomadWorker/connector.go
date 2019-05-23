e350e568562b22d696c74a64b149d3b30bb03247
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-05-23 23:33:49 +0200

    .Update set initial count to 100

--- nomadWorker/connector.go
+++ nomadWorker/connector.go
@@ -7,6 +7,8 @@ import (
 // Connector is a object that allows to interact with nomad worker
 type Connector struct {
 	log zerolog.Logger
+
+	currentCount uint
 }
 
 // Config contains the main configuration for the nomad worker connector
@@ -19,6 +21,8 @@ func (cfg *Config) New() (*Connector, error) {
 
 	nc := &Connector{
 		log: cfg.Logger,
+		// HACK: Set it to 100 for now to ensure at startup that a scale is possible (i.e. with a value 0 a initial downscale would be ignored)
+		currentCount: 100,
 	}
 
 	cfg.Logger.Info().Msg("Setting up nomad worker connector ... done")
