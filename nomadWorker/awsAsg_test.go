d954158e068e63f0fe45be4e54d559942d17efd7
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-05-25 20:28:54 +0200

    .Update added filter for ASG by key

--- nomadWorker/awsAsg_test.go
+++ nomadWorker/awsAsg_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/autoscaling"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetTagValue(t *testing.T) {
@@ -46,4 +47,45 @@ func TestGetTagValue(t *testing.T) {
 	value, err = getTagValue("key", tags)
 	assert.Error(t, err)
 	assert.Empty(t, value)
+}
+
+func TestGetAutoScalingGoupByKey(t *testing.T) {
+
+	var autoScalingGroups []*autoscaling.Group
+
+	// none, empty
+	asg := filterAutoScalingGroupByKey("key", "value", autoScalingGroups)
+	assert.Nil(t, asg)
+
+	// none, no match
+	asgIn := autoscaling.Group{}
+	autoScalingGroups = append(autoScalingGroups, &asgIn)
+	asg = filterAutoScalingGroupByKey("key", "value", autoScalingGroups)
+	assert.Nil(t, asg)
+
+	// found, match
+	autoScalingGroups = make([]*autoscaling.Group, 0)
+	key := "datacenter"
+	tagVal := "private-services"
+	asgName := "my-asg"
+	var tags []*autoscaling.TagDescription
+	td := autoscaling.TagDescription{Key: &key, Value: &tagVal}
+	tags = append(tags, &td)
+	asgIn = autoscaling.Group{Tags: tags, AutoScalingGroupName: &asgName}
+	autoScalingGroups = append(autoScalingGroups, &asgIn)
+	asg = filterAutoScalingGroupByKey(key, tagVal, autoScalingGroups)
+	require.NotNil(t, asg)
+	assert.Equal(t, asgName, *asg.AutoScalingGroupName)
+
+	// not found, no match
+	autoScalingGroups = make([]*autoscaling.Group, 0)
+	autoScalingGroups = append(autoScalingGroups, &asgIn)
+	asg = filterAutoScalingGroupByKey(key, "tagVal", autoScalingGroups)
+	assert.Nil(t, asg)
+
+	// robust against nil
+	autoScalingGroups = make([]*autoscaling.Group, 0)
+	autoScalingGroups = append(autoScalingGroups, nil)
+	asg = filterAutoScalingGroupByKey(key, "tagVal", autoScalingGroups)
+	assert.Nil(t, asg)
 }
