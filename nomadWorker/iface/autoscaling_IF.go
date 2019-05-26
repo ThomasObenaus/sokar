bca5c3b11eb221a4682046631cd4ecb16305d04f
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-05-26 20:14:34 +0200

    .Update implemented IsJobDead

--- nomadWorker/iface/autoscaling_IF.go
+++ nomadWorker/iface/autoscaling_IF.go
@@ -1,8 +1,15 @@
 package nomadWorker
 
-import "github.com/aws/aws-sdk-go/service/autoscaling"
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+)
 
 // AutoScaling is the minimal interface needed to interact with aws autoscaling
 type AutoScaling interface {
 	DescribeAutoScalingGroups(input *autoscaling.DescribeAutoScalingGroupsInput) (*autoscaling.DescribeAutoScalingGroupsOutput, error)
+}
+
+type AutoScalingFactory interface {
+	CreateAutoScaling(session *session.Session) AutoScaling
 }
