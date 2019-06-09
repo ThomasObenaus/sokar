396e23effbbcdc9064848db2d3bd643c7e2f9292
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-06-09 19:29:12 +0200

    .Update added method for linear planning mode

--- capacityPlanner/planLinear.go
+++ capacityPlanner/planLinear.go
@@ -1,5 +1,31 @@
 package capacityPlanner
 
-func planLinear(scaleFactor float32, currentScale uint) uint {
-	return 0
+import (
+	"math"
+
+	"github.com/thomasobenaus/sokar/helper"
+)
+
+func (cp *CapacityPlanner) planLinear(scaleFactor float32, currentScale uint) uint {
+
+	if scaleFactor == 0 {
+		return currentScale
+	}
+
+	increment := float64(scaleFactor * float32(currentScale))
+	incrementInt := int(math.Ceil(increment))
+
+	// at least scale up by one if scaleFactor is positive
+	if incrementInt == 0 && scaleFactor > 0 {
+		incrementInt = 1
+	}
+
+	// at least scale down by one if scaleFactor is negative
+	if incrementInt == 0 && scaleFactor < 0 {
+		incrementInt = -1
+	}
+
+	plannedScale := helper.IncUint(currentScale, incrementInt)
+
+	return plannedScale
 }
