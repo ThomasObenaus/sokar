984b53bf08f3d6b601fc95a813a1544d2e7cf27d
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-09-01 11:36:11 +0200

    .Update move to machine image
    Instead of using docker now a plain linux image is used to allow to run docker compose on it
    
    
    cfg 1
    
    
    cfg 2
    
    
    cfg 3
    
    
    cfg 4
    
    
    cfg 5
    
    
    cfg 6
    
    
    cfg 7
    
    
    cfg 8
    
    
    cfg 9
    
    
    cfg 10
    
    
    .Ignore vendor
    
    
    .Update added Gopk.lock
    
    
    .Update added Gopkg.lock for test and ignore vendor in docker
    
    
    .Update removed src_dir from docker build call

--- test/service_test/app_test.go
+++ test/service_test/app_test.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestSimple(t *testing.T) {
+func Test_ConfigEndpoint(t *testing.T) {
 
-	assert.NotNil(t, nil)
+	resp, err := http.Get("http://localhost:11000/api/config")
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
 }
