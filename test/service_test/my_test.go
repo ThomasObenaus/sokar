385fba082d0d7bb150dba365126b298e66e4eb96
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-10-20 10:16:43 +0200

    clenaup

--- test/service_test/my_test.go
+++ test/service_test/my_test.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 	"testing"
 	"time"
@@ -21,79 +19,11 @@ import (
 
 func Test_My(t *testing.T) {
 
-	//ctx := myCTX{donechan: make(chan struct{}, 1)}
-
-	//mockCtrl := gomock.NewController(t)
-
-	ctx := context.Background()
-	//ctx, cn := context.WithTimeout(ctx, time.Second*10)
-	//_ = cn
-
-	mockCtrl, ctx := gomock.WithContext(ctx, t)
-
-	go func() {
-		fmt.Println("STAAAAAAAART")
-
-		for {
-			select {
-			case <-ctx.Done():
-				fmt.Printf("EEEEEEEEEEEEEEEND %s\n", ctx.Err().Error())
-				return
-			}
-		}
-	}()
-
-	//mockCtrl := gomock.NewController(t)
+	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
 	mock := NewMockHTTP(mockCtrl, 18000)
-	_ = mock
-	//mock.GET("jj")
-
 	mock.EXPECT().GET("/health").Return(http.StatusOK, "BLA")
-	//mock.EXPECT().GET("/api/alerts").Return(http.StatusNotFound, "BLA")
 
-	//server := New(18000, t)
-	//defer server.Close()
-	//
-	//server.GET("/health", http.StatusOK, "SUPER")
-	//
 	time.Sleep(time.Second * 5)
-
-	//mockCtrl := gomock.NewController(t)
-	//defer mockCtrl.Finish()
-	//mock := NewMockHTTP(mockCtrl)
-	//
-	//mock.EXPECT().POST("HELLO").Return(http.StatusOK, "huhu")
-	//
-	//receiver := api.New(18000)
-	//receiver.Run()
-	//// receiver.GET(/health,response)
-	//receiver.Router.HandlerFunc("GET", "/health", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	//
-	//	code, _ := mock.POST("HELLO")
-	//	w.Header().Set("Content-Type", "application/json")
-	//	w.WriteHeader(code)
-	//	io.WriteString(w, "data")
-	//
-	//	//receiver.Stop()
-	//}))
-	//time.Sleep(time.Millisecond * 100)
-	//
-	//res, err := http.Get("http://127.0.0.1:18000/health")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//greeting, err := ioutil.ReadAll(res.Body)
-	//res.Body.Close()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//fmt.Printf("%s\n", greeting)
-	//
-	////receiver.Join()
-	//receiver.Stop()
-
 }
