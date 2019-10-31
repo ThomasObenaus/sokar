fc234c523958265a700bb38a27745c7935a4a347
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-10-31 13:40:37 +0100

    .Update added test

--- test/service_test/my_test.go
+++ test/service_test/my_test.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"net/http"
 	"testing"
 	"time"
+
+	"github.com/thomasobenaus/sokar/api"
 )
 
 // # Notes:
@@ -28,4 +33,90 @@ func Test_My(t *testing.T) {
 		awsMock.EXPECT().GET("/health").Within(time.Second*10).Return(NewStringResponse("{\"Call\": 2}", AddHeader("Content-Type", "application/protobuf"))),
 		nomadMock.EXPECT().GET("/health").Within(time.Second*10).Return(NewStringResponse("{\"Call\": 3}", AddHeader("Content-Type", "application/json"))),
 	)
+}
+
+func Test_Job(t *testing.T) {
+
+	ctrl := NewController(t)
+	defer ctrl.Finish()
+
+	nomadMock := NewMockHTTP(ctrl, 18000, FailOnUnexpectedCalls(false))
+
+	InOrder(
+		time.Now(),
+		nomadMock.EXPECT().GET("/v1/job/fail-service").Within(time.Second*50).Return(NewStringResponse("{\"Call\": 1}", AddHeader("Content-Type", "application/json"))),
+	)
+
+	//sendAlert("http://127.0.0.1:11000/api/alerts")
+}
+
+func Test_Jow(t *testing.T) {
+
+	server := api.New(18000)
+	server.Run()
+
+	counter := 0
+
+	server.Router.HandlerFunc("GET", "/v1/job/fail-service", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("[%d] Request %v\n", counter, r)
+		counter++
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("{\"Call\": 1}"))
+	})
+
+	time.Sleep(time.Second * 30)
+}
+
+func sendAlert(url string) {
+	data := []byte(`{
+		"receiver": "PM",
+		"status": "firing",
+		"alerts": [
+		  {
+			"status": "firing",
+			"labels": {
+			  "alertname": "AlertA",
+			  "alert-type": "scaling",
+			  "scale-type": "up"
+			},
+			"annotations": {
+			  "description": "Scales the component XYZ UP"
+			},
+			"startsAt": "2019-02-23T12:00:00.000+01:00",
+			"endsAt": "2019-02-23T12:05:00.000+01:00",
+			"generatorURL": "http://generator_url"
+		  },
+		  {
+			"status": "firings",
+			"labels": {
+			  "alertname": "AlertB",
+			  "alert-type": "scaling",
+			  "scale-type": "down"
+			},
+			"annotations": {
+			  "description": "Scales the component XYZ DOWN"
+			},
+			"startsAt": "2019-02-23T12:00:00.000+01:00",
+			"endsAt": "2019-02-23T12:05:00.000+01:00",
+			"generatorURL": "http://generatorURL"
+		  }
+		],
+		"groupLabels": {},
+		"commonLabels": { "alertname": "AlertA" },
+		"commonAnnotations": {},
+		"externalURL": "http://externalURL",
+		"version": "4",
+		"groupKey": "{}:{}"
+	  }`)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
 }
