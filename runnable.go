08154ce4ed206fee98d55b90df4945d1cd679bfe
Thomas Obenaus <obenaus.thomas@gmail.com>
2020-05-26 12:57:03 +0200

    .Update adjusted Runnable to interface in go-base/shutdown

--- runnable.go
+++ runnable.go
@@ -8,18 +8,18 @@ import (
 
 // Runnable is a interface for runnable sokar components
 type Runnable interface {
-	Run()
+	Start()
 	Join()
 	Stop() error
-	GetName() string
+	String() string
 }
 
 // Run calls Run() on all Runnables in the list as they are ordered there.
 func Run(orderedRunnables []Runnable, logger zerolog.Logger) {
 	for _, runnable := range orderedRunnables {
-		name := runnable.GetName()
+		name := runnable.String()
 		logger.Debug().Msgf("Starting %s ...", name)
-		runnable.Run()
+		runnable.Start()
 		logger.Info().Msgf("%s running.", name)
 	}
 }
@@ -27,7 +27,7 @@ func Run(orderedRunnables []Runnable, logger zerolog.Logger) {
 // Join calls Join() on all Runnables in the list as they are ordered there.
 func Join(orderedRunnables []Runnable, logger zerolog.Logger) {
 	for _, runnable := range orderedRunnables {
-		name := runnable.GetName()
+		name := runnable.String()
 		logger.Debug().Msgf("Join %s ...", name)
 		runnable.Join()
 		logger.Debug().Msgf("Join %s ... done.", name)
@@ -38,7 +38,7 @@ func Join(orderedRunnables []Runnable, logger zerolog.Logger) {
 func Stop(orderedRunnables []Runnable, logger zerolog.Logger) {
 	for i := len(orderedRunnables) - 1; i >= 0; i-- {
 		runnable := orderedRunnables[i]
-		name := runnable.GetName()
+		name := runnable.String()
 		logger.Debug().Msgf("Stopping %s ...", name)
 		err := runnable.Stop()
 		if err != nil {
